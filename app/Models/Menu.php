0216e8b46106d5cd79a6feb5cbcbe49d09fc7dac
Rachmat Kurniawan <kurachmat276@gmail.com>
2022-06-27 08:11:54 +0700

    update

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use SebastianBergmann\GlobalState\Restorer;
 
 class Menu extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function categoory()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function restorant()
+    {
+        return $this->belongsTo(Restaurant::class, 'restaurant_id');
+    }
 }
