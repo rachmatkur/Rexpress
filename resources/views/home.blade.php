2de2802818c33ca6fd9d2ef8ab93df5a7c1050f6
ethan <ethanwangtki1@gmail.com>
2022-06-29 11:20:52 +0700

    edited homepage view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage333</title>
-</head>
-<body>
+@extends('master')
+
+@section('title','homepage')
     
-</body>
-</html>
+@section('content')
+    <div class="container-fluid bg-light m-0 p-0 position-relative">
+        <img src="/gambar/banner.jpg" alt="" class="background">
+        <div class ="contentContainer">
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="/gambar/sayur.png" class="d-block w-100       " alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h1>SAYUR</h1>
+                      <p>Sayur atau sayuran merupakan sebutan umum bagi bahan pangan nabati yang biasanya mengandung kadar air yang tinggi, yang dapat dikonsumsi setelah dimasak atau diolah dengan teknik tertentu, atau dalam keadaan segar.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item" data-bs-interval="2000">
+                    <img src="/gambar/telor.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h1>TELUR</h1>
+                      <p>Telur adalah wadah organik yang berisi zigot di mana embrio berkembang sampai dapat bertahan hidup sendiri, di mana hewan menetas. Telur dihasilkan dari pembuahan sel telur. </p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/gambar/burger.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h1>BURGER</h1>
+                      <p>Hamburger adalah sejenis roti berbentuk bundar yang diiris dua, dan di tengahnya diisi dengan patty yang biasanya diambil dari daging, kemudian sayur-sayuran berupa selada, tomat dan bawang bombai. </p>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,13 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing: border-box;
+        }
+        .background{
+            -webkit-mask-image: linear-gradient(
+            to top,
+            rgba(0, 0, 0, 0),
+            rgba(0, 0, 0, 1)
+          );
+          width: 100%;
+          height: 100%;
+        }
+        .contentContainer {
+            position: absolute; top: 1%;
+            margin-left: auto;
+            margin-right: auto;
+            left: 0;
+            right: 0;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .carousel-caption {
+            top: 0;
+            bottom: auto;
+        }
+    </style>
     <script src="{{ asset('js/app.js') }}"></script>
-    <title>Document</title>
+    <title>@yield('title')</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark d-flex justify-content-evenly">
-        <ul class="navbar-nav w-25 d-flex justify-content-evenly">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark d-flex justify-content-evenly align-items-center">
+        <a href="" class="nav-brand"><img src="/gambar/logo.webp" alt="" style="height: 50px; width: 100px;"></a>
+        <ul class="navbar-nav w-25 d-flex justify-content-evenly h-100"> 
             <li class="nav-item">
                 <a href="" class="nav-link">Home</a>
             </li>
@@ -34,6 +66,7 @@
             </ul>
         </div>
     </nav>
+    @yield('content')
 </body>
 
 </html>
