5757ce0a673ac16879e4a22c352d77f7bd4fb1b6
ethan <ethanwangtki1@gmail.com>
2022-06-29 21:04:49 +0700

    edit migration 2

--- database/migrations/2022_06_23_140002_create_menus_table.php
+++ database/migrations/2022_06_23_140002_create_menus_table.php
@@ -20,7 +20,7 @@ class CreateMenusTable extends Migration
             $table->text('description');
             $table->integer('stock');
             $table->foreignId('category_id')->constrained();
-            $table->foreignId('restaurant_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
