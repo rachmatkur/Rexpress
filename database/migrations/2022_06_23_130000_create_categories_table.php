12ba58d953d3f47f61122020e76241feaf9dfab9
ethan <ethanwangtki1@gmail.com>
2022-07-03 14:27:54 +0700

    added user_id column to categories table

--- database/migrations/2022_06_23_130000_create_categories_table.php
+++ database/migrations/2022_06_23_130000_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->string('category');
             $table->timestamps();
         });
